45874a0527300256c5e2196bd43a7b6170274c58
Chi Ting <tnnd6666@gmail>
2022-10-16 18:37:48 +0800

    Code done

--- vendor.py
+++ vendor.py
@@ -1,32 +1,44 @@
 import json
 import yaml
+import datetime
+
+def valid_doc(money):
+    with open('inventories.json', 'r', encoding='utf-8') as f:
+        data = json.load(f)
+
+    # output product
+    for i in data:
+        if (money >= i['price']) & (i['number'] > 0):
+            print("name:" + i['name'])
+            print("price:" + str(i['price']))
+            print("number:" + str(i['number']) +'\n')
+
+    f.close()
+
+def output(buy_doc):
+    with open('inventories.json', 'r', encoding='utf-8') as f:
+        data = json.load(f)
+        # output money
+        print('找零' + str(money - data[buy_doc]['price']))
+
+        # reload json
+        data[buy_doc]['number'] = data[buy_doc]['number'] - 1
+        with open('inventories.json', 'w', encoding='utf-8') as file:
+            json.dump(data, file)
+        file.close()
+
+        # record yaml
+        recording = {'name': data[buy_doc]['name'], 'price': data[buy_doc]['price'], 'time': datetime.datetime.now()}
+
+        with open('history.yaml', 'w', encoding='utf-8') as file:
+            yaml.dump(recording, file)
+        file.close()
+
+        f.close()
 
 
 money = int(input('輸入金額'))
-
-with open('inventories.json', 'r' , encoding='utf-16') as f:
-  data = json.load(f)
-
-for i in data:
-  if (money >= i['price']) & (i['number'] > 0):
-    print("name:" + i['name'])
-    print("price:" + str(i['price']))
-    print("number:" + str(i['number']))
-
+valid_doc(money)
 buy_doc = int(input('輸入商品編號'))
+output(buy_doc)
 
-print('找零' + str(money - data[buy_doc]['price']) )
-data[buy_doc]['number'] = data[buy_doc]['number']-1
-
-with open('inventories.json', 'w' , encoding='utf-16') as f:
-    json.dump(data, f)
-    f.close()
-
-recording = """
-- 'eric'
-- 'justin'
-- 'mary-kate'
-"""
-
-with open('history.yaml', 'w') as file:
-    prime_service = yaml.safe_load(file)
